abbf06757b61f7258b47d3247632481afde1bd05
Marc-Antoine Arnaud <marc.antoine@nomalab.com>
2017-03-20 15:33:39 +0100

    remove old files

--- src/reader.rs
+++ src/reader.rs
@@ -1,12 +1,8 @@
 
-// use std::fs::File;
-// use std::io::SeekFrom;
-
 pub trait Reader {
   fn open(filename: &String) -> Self;
   // fn exists(&self, filename: &String) -> bool;
   // fn open(&mut self, filename: String) -> Result<Reader, String>;
-
 
   fn get_cache_size(&self) -> Option<usize>;
   fn set_cache_size(&mut self, cache_size: Option<usize>);
@@ -17,49 +13,3 @@ pub trait Reader {
   fn read(&mut self, size: usize) -> Result<Vec<u8>, String>;
   // fn seek(&mut self, seek: SeekFrom) -> Result<u64, String>;
 }
-
-
-// impl Reader {
-//   pub fn get_type(&self) -> ReaderKind {
-//     match self.mode {
-//       ReaderKind::Http => ReaderKind::Http,
-//       ReaderKind::FileSystem => ReaderKind::FileSystem
-//     }
-//   }
-
-//   pub fn get_cache_size(&self) -> Option<usize> {
-//     self.cache_size
-//   }
-
-//   pub fn set_cache_size(&mut self, cache_size: Option<usize>) {
-//     self.cache_size = cache_size;
-//   }
-
-//   pub fn read(&mut self, size: usize) -> Result<Vec<u8>, String> {
-//     match self.mode {
-//       ReaderKind::Http => http::read(self, size),
-//       ReaderKind::FileSystem => file_system::read(self, size)
-//     }
-//   }
-
-//   pub fn get_position(&mut self) -> Result<u64, String> {
-//     match self.mode {
-//       ReaderKind::Http => http::get_position(self),
-//       ReaderKind::FileSystem => file_system::get_position(self)
-//     }
-//   }
-
-//   pub fn get_size(&mut self) -> Result<u64, String> {
-//     match self.mode {
-//       ReaderKind::Http => http::get_size(self),
-//       ReaderKind::FileSystem => file_system::get_size(self)
-//     }
-//   }
-
-//   pub fn seek(&mut self, seek: SeekFrom) -> Result<u64, String> {
-//     match self.mode {
-//       ReaderKind::Http => http::seek(self, seek),
-//       ReaderKind::FileSystem => file_system::seek(self, seek)
-//     }
-//   }
-// }
